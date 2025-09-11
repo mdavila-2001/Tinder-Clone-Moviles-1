e803d84f1d37de7fa4f65e92a8d75cb14c6793ed
Marcelo Dávila FOS <marcelo.tester.fos@gmail.com>
2025-09-11 11:49:52 -0400

    refactor: Remove unused imports in InterestCard and InterestList components

--- app/src/main/java/com/mdavila_2001/practico_1_moviles_marcelo_davila/components/InterestList.kt
+++ app/src/main/java/com/mdavila_2001/practico_1_moviles_marcelo_davila/components/InterestList.kt
@@ -6,18 +6,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
 import com.mdavila_2001.practico_1_moviles_marcelo_davila.models.Interest
-import kotlin.times
-import kotlin.unaryMinus
 
 @Composable
 fun InterestList(
